5e1d8c04407ea135ef6b7af7c35fb9394dd4abbd
V-Amit Chawla <Amit.Chawla@blackbaud.me>
2023-06-22 13:28:00 -0400

    add angular project for identity server

--- InsuranceClaimPortal/Services/YCompanyUserManagementAPI/YCompany.UserManagement.Infrastructure/Infrastructure.cs
+++ InsuranceClaimPortal/Services/YCompanyUserManagementAPI/YCompany.UserManagement.Infrastructure/Infrastructure.cs
@@ -1,4 +1,6 @@
-﻿namespace YCompany.UserManagement.Infrastructure
+﻿using YCompany.UserManagement.Domain.InfrastructureInterfaces;
+
+namespace YCompany.UserManagement.Infrastructure
 {
     public class Infrastructure : IInfrastructure
     {
